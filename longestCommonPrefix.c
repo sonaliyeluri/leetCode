26f4abef54ab9735af4f60d2972ce728f8e83cf0
Sonali Yeluri <sonaliyeluri@yahoo.com>
2018-10-18 11:09:52 -0700

    Longest Common Prefix
    Write a function to find the longest common prefix string amongst an array of strings.
    
    If there is no common prefix, return an empty string "".
    
    Example 1:
    
    Input: ["flower","flow","flight"]
    Output: "fl"
    Example 2:
    
    Input: ["dog","racecar","car"]
    Output: ""
    Explanation: There is no common prefix among the input strings.

---
+++ longestCommonPrefix.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define INT_MAX 2147483647
+
+int findMinLen(char strs[][7], int n) {
+	int len = INT_MAX;
+	for (int i=0;i<n;i++)
+		if(len > strlen(strs[i]))
+			len = strlen(strs[i]);
+	return len;
+}
+
+int isPrefix (char strs[][7], char ch, int i, int n) {
+	int flag = 1;
+	for(int j=0;j<n;j++)
+		if (strs[j][i] != ch)
+			flag = 0;
+	return flag?1:0;
+}
+
+char *commonPrefix (char strs[][7], int n) {
+	int len = findMinLen(strs,n);
+	int prefix;
+	char res[len];
+	int j = 0;
+	char ch;
+	for (int i=0;i<len;i++) {
+		ch = strs[0][i];
+		prefix = isPrefix(strs,ch,i,n);
+		if (prefix == 1){
+			res[j] = ch;
+			j++;
+		}
+		if (i == 0 && prefix != 1)
+			return "";
+	}
+	char pre[j];
+	for(int i=0;i<j;i++) 
+		pre[i] = res[i];
+	return pre;
+}	
+
+int main() {
+	char strs[][7] = {"flower","flow","float"};
+	printf("Common Prefix %s",commonPrefix(strs,3));
+	printf("\n");
+	return 0;
+}  
