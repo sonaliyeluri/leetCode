d55801684da3ea2a3d2288027957067f27d4a3e6
Sonali Yeluri <sonaliyeluri@yahoo.com>
2018-10-20 15:22:04 -0700

    Strobogrammatic Number
    A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
    
    Write a function to determine if a number is strobogrammatic. The number is represented as a string.
    
    Example 1:
    
    Input:  "69"
    Output: true
    Example 2:
    
    Input:  "88"
    Output: true
    Example 3:
    
    Input:  "962"
    Output: false

---
+++ strobogrammaticNumber.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isStrobogrammatic(char *num) {
+	int n = strlen(num);
+	int l = 0;
+	int r = n-1;
+	while (l <= r) {
+		if (num[l] == num[r]) {
+			if (num[l] != '1' && num[l] != '0' && num[l] != '8')
+				return false;
+		}
+		else {
+			if ((num[l] != '6' || num[r] != '9') && (num[l] != '9' && num[r] != '6'))
+				return true;
+		}
+		l++;
+		r--;
+	}
+	return true;
+}
+
+int main() {
+	//char s1[2] = "88";
+	//printf("%s - %d\n",s1, isStrobogrammatic(s1));
+	//char s2[2] = "22";
+	//printf("%s - %d\n",s2, isStrobogrammatic(s2));
+	//char s3[3] = "101";
+	//printf("%s - %d\n",s3, isStrobogrammatic(s3));
+	char s4[3] = "609";
+	printf("%s - %d\n",s4, isStrobogrammatic(s4));
+	//char s5[3] = "818";
+	//printf("%s - %d\n",s5, isStrobogrammatic(s5));
+	return 0;
+}
